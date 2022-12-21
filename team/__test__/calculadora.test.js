c18c61ce1a048c3eff3791d9386666718d3fc541
Marçal <chaiben79@gmail.com>
2022-12-21 10:36:05 +0100

    TDD team compleated

--- team/__test__/calculadora.test.js
+++ team/__test__/calculadora.test.js
@@ -21,9 +21,35 @@ test("recibe null y retorna 0", () => {
   expect(sumar(null)).toBe(0);
 })
 
+
 test("sumar 1 + 2 + 4 + 10 debaria devolver 17", () => {
   expect(sumar("1,2,4,10")).toBe(17)
 })
 test("sumar 1 + 2 + 4 + 10 con espacios debaria devolver 17", () => {
   expect(sumar(" 1, 2, 4, 10 ")).toBe(17)
 })
+
+test("1n2n3 shoud return 6", () => {
+  expect(sumar("1n2n3")).toBe(6);
+})
+
+test("Seleccionando el delimitador deseado en la posición //;\n1;2, sea igual a 3", () => {
+  expect(sumar("//;\n1;2")).toBe(3);
+})
+
+test("Seleccionando el delimitador deseado en la posición //.\n1.2, sea igual a 3", () => {
+  expect(sumar("//.\n1.2")).toBe(3);
+})
+
+test("El string contiene numeros negativos y devuelve error", () => {
+  expect(sumar("-10")).toBe("Los números negativos no están permitidos. -10");
+});
+
+test("El string contiene numeros negativos y devuelve error 2", () => {
+  expect(sumar("-10,2,-20,7")).toBe("Los números negativos no están permitidos. -10,-20");
+});
+
+// test("El string contiene al menos un número negativo y devuelve error 2", () => {
+//   expect(sumar("-10,2,-20,7")).toThrow('Los números negativos no están permitidos. -10,-20');
+
+// });
--- team/calculadora.js
+++ team/calculadora.js
@@ -2,16 +2,37 @@
 const sumar = (str) => {
   
   str = (typeof(str) !== "string") ? "" : str
+  let delimitador = "n";
+  if (str[0] === "/" && str[1] === "/" && str.includes("\n")) {
+    // Captura el delimitador
+
+    delimitador = str[2];
+    str = str.substr(3);
+     
+  }
+  console.log(delimitador);
+  str = str.replaceAll(delimitador, ",");
   const numeros = str.split(',');
+  
 
   let res = 0;
+  const numerosNegativos = [];
+
   numeros.forEach(numero => {
     
-    res += Number(numero);
+    let n = Number(numero);
+    if (n < 0) {
+        numerosNegativos.push(n);
+    } else {
+        if (n > 1000) 
+          return
+        res += n;
+    };
 
   });
-
-  
+  if (numerosNegativos.length > 0) {
+    return 'Los números negativos no están permitidos. '+ numerosNegativos.join(",")
+  }
   return res;
   
 }
